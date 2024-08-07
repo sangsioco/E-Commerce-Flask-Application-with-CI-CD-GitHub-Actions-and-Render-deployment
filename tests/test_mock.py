06e3760d80c7a7ffc0f90a1db2efe6268fdb6991
sangsioco <sangsioco@gmail.com>
2024-08-07 05:17:16 -0700

    editing pytest

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,24 +1,23 @@
-import unittest
-from unittest.mock import MagicMock, patch
+import pytest
+from unittest.mock import MagicMock
 from faker import Faker
 from werkzeug.security import generate_password_hash
-from services.customerAccountService import login_customer
 
-class TestLogInCustomer(unittest.TestCase):
 
-    @patch('services.customerAccountService.db.session.execute')
-    def test_login_customer(self, mock_execute):
-        faker = Faker()
-        mock_user = MagicMock()
-        mock_user.id = 1
-        mock_user.roles = [MagicMock(role_name='admin'), MagicMock(role_name='user')]
-        password = faker.password()
-        mock_user.username = faker.user_name()
-        mock_user.password = generate_password_hash(password)
-        mock_execute.return_value.scalar_one_or_none.return_value = mock_user
-        
-        response = login_customer(mock_user.username, password)
-        self.assertEqual(response['status'], 'success')
+@pytest.fixture
+def faker_instance():
+    return Faker()
+
+@pytest.fixture
+def mock_user(faker_instance):
+    mock_user = MagicMock()
+    mock_user.id = 1
+    mock_user.roles = [MagicMock(role_name='admin'), MagicMock(role_name='user')]
+    password = faker_instance.password()
+    mock_user.username = faker_instance.user_name()
+    mock_user.password = generate_password_hash(password)
+    return mock_user, password
+
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main([__file__])
