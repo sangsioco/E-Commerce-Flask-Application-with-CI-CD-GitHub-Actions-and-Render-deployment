931eb905ae1492c5db7cd2ba5228d7319b7d0d58
sangsioco <sangsioco@gmail.com>
2024-08-07 06:31:55 -0700

    updating tests/test_mock

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
 from faker import Faker
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @pytest.fixture
@@ -18,6 +18,18 @@ def mock_user(faker_instance):
     mock_user.password = generate_password_hash(password)
     return mock_user, password
 
+def test_mock_user_creation(mock_user):
+    user, password = mock_user
+    assert user.id == 1
+    assert len(user.roles) == 2
+    assert user.roles[0].role_name == 'admin'
+    assert user.roles[1].role_name == 'user'
+    assert check_password_hash(user.password, password)
+    
+def test_mock_user_username(mock_user, faker_instance):
+    user, _ = mock_user
+    assert user.username is not None
+    assert isinstance(user.username, str)
 
 if __name__ == '__main__':
     pytest.main([__file__])
