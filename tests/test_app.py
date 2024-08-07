a5df1703f2343846cc40cd8b13f99c0aad4bf629
sangsioco <sangsioco@gmail.com>
2024-08-07 12:04:43 -0700

    updating test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,16 +6,25 @@ from services.customerAccountService import login_customer
 
 class TestLogInCustomer(unittest.TestCase):
 
-    @patch('services.customerAccountServices.db.session.execute')
+    @patch('services.customerAccountService.db.session.execute')
     def test_login_customer(self, mock_customer):
         faker = Faker()
         mock_user = MagicMock()
         mock_user.id = 1
-        mock_user.roles = [MagicMock(role_names='admin'), MagicMock(role_name='user')]
+        mock_user.roles = [MagicMock(role_name='admin'), MagicMock(role_name='user')]
         password = faker.password()
         mock_user.username = faker.user_name()
         mock_user.password = generate_password_hash(password)
         mock_customer.return_value.scalar_one_or_none.return_value = mock_user
-        
+
+        # Call the login_customer function with the mock data
+        result = login_customer(mock_user.username, password)
+
+        # Perform assertions to check the result
+        self.assertIsNotNone(result)
+        self.assertEqual(result.id, mock_user.id)
+        self.assertEqual(result.username, mock_user.username)
+        self.assertTrue(result.check_password(password))
+
 if __name__ == '__main__':
     unittest.main()
