ecbd35b26a8123b340951913af215fbf90e48f00
sangsioco <sangsioco@gmail.com>
2024-08-07 07:22:34 -0700

    added line 6 to 10 in cofig.py

--- config.py
+++ config.py
@@ -2,3 +2,9 @@ class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:CIAjrj202709*!@localhost/advance_e_commerce_db'
     CACHED_TYPE = 'SimpleCache'
     DEBUG = True
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use an in-memory SQLite database
+    DEBUG = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
