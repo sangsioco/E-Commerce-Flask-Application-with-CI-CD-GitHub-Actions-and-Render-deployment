afc64f1255c2ff8284520c9e529f28cfb93ad2d9
sangsioco <sangsioco@gmail.com>
2024-08-11 15:55:31 -0700

    updating files for postgresql

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:CIAjrj202709*!@localhost/advance_e_commerce_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://module_16_miniproject_user:X1pL8PjYnTMaeP7k9DXXCufBmi67fAuT@dpg-cqsjvllumphs73d0uhh0-a.oregon-postgres.render.com/module_16_miniproject'
+    SQLALCHEMY_TRACK_MODIFICATION = False
     CACHED_TYPE = 'SimpleCache'
     DEBUG = True
